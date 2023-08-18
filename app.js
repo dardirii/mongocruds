9772bad2c877f7efe7a586bffe17e2083780a0d3
unknown <dardirii>
2023-08-18 11:20:43 +0700

    committed

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const log = require('./middlewares/logger')
 app.use(log);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use('public', express.static(path.join(__dirname, uploads)));
 app.use(router);
 app.use((req, res, next) => {
     res.status(404);
